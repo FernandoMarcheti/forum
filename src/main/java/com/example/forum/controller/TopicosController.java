41c93ad0f977de3cde40d674e62115fdf60b882d
Fernando Marcheti <fernandomarcheti@gmail.com>
2019-11-30 12:56:18 -0300

    Criacao de form de topicos, criacao de repository de curso, criacao end-point para salvar um novo topico

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,14 +1,21 @@
 package com.example.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.forum.controller.dto.TopicoDto;
+import com.example.forum.controller.form.TopicoForm;
 import com.example.forum.model.Topico;
+import com.example.forum.repository.CursoRepository;
 import com.example.forum.repository.TopicoRepository;
 
 @RestController
@@ -17,6 +24,9 @@ public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
 	
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
@@ -28,5 +38,14 @@ public class TopicosController {
 		}
 		
 		return TopicoDto.converte(topicos);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converte(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 }

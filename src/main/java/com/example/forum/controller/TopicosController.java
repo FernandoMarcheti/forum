969f6abe45ed3996ae2fb56c59fe03ab3cb9f334
Fernando Marcheti <fernandomarcheti@gmail.com>
2019-11-30 11:10:21 -0300

    Criacao do Repository de Topicos e criacao de end-point para listar topicos e listar topicos por nome do curso

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,24 +1,32 @@
 package com.example.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.forum.controller.dto.TopicoDto;
-import com.example.forum.model.Curso;
 import com.example.forum.model.Topico;
+import com.example.forum.repository.TopicoRepository;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@GetMapping
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Titulo 1", "Mensagem título 1", new Curso("Spring", "Programação"));
+	public List<TopicoDto> lista(String nomeCurso) {
+		List<Topico> topicos = null;
+		if(nomeCurso == null) {
+			topicos = topicoRepository.findAll();
+		} else {
+			topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+		}
 		
-		return TopicoDto.converte(Arrays.asList(topico));
+		return TopicoDto.converte(topicos);
 	}
 }
